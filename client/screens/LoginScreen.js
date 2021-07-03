b81baa4920885c8b9f899d2051f08b9c0980767b
Brittany Deventer <bdvntr@gmail.com>
2021-07-03 15:52:39 -0700

    Logo link on login page disabled temporarily.

--- client/screens/LoginScreen.js
+++ client/screens/LoginScreen.js
@@ -69,7 +69,7 @@ const LoginScreen = ({ navigation }) => {
         </View>
 
         <View>
-          <TouchableOpacity onPress={() => navigation.navigate("About")}>
+          <TouchableOpacity>
             <Image
               source={require("../assets/Leaf-Logo.png")}
               style={styles.logo}
