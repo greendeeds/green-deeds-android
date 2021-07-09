6ac13f8fb182343faf41d0c09c8b866126984130
Jessie-Mills <jessiemills11@gmail.com>
2021-07-09 09:00:46 -0400

    updated package-lock; fixed about navigation on compost and recycle screens; same screens need logout functionality in footer addressed

--- client/screens/RecycleScreen.js
+++ client/screens/RecycleScreen.js
@@ -2,35 +2,35 @@ import React from "react";
 import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
 import ActivityBanner from "../components/ActivityBanner";
 import ScanBanner from "../components/ScanBanner";
-import { Entypo } from '@expo/vector-icons';
+import { Entypo } from "@expo/vector-icons";
 import { Spacing, Typography } from "../Styles";
 import { TouchableOpacity } from "react-native-gesture-handler";
-
 
 export default class RecycleScreen extends React.Component {
   state = {
     recycled: 0,
-    redeemableAmount: "0.00"  //route.params !== undefined ? route.params.redeemableAmount : "0.00"
-  }
+    redeemableAmount: "0.00", //route.params !== undefined ? route.params.redeemableAmount : "0.00"
+  };
 
-  photo = () => this.props.navigation.navigate("Camera")
-  celo = () => this.props.navigation.navigate("Celo", { redeemableAmount: this.state.redeemableAmount });
-  logout = () => this.props.navigation.navigate("logoutAction")
-  about = () => this.props.navigation.navigate("about")
+  photo = () => this.props.navigation.navigate("Camera");
+  celo = () =>
+    this.props.navigation.navigate("Celo", {
+      redeemableAmount: this.state.redeemableAmount,
+    });
+  logout = () => this.props.navigation.navigate("logoutAction");
+  about = () => this.props.navigation.navigate("About");
 
   photoPressed = () => {
     this.setState({
       recycled: 3,
-      redeemableAmount: "3.00"
-    })
-
-  }
+      redeemableAmount: "3.00",
+    });
+  };
 
   render() {
     return (
       <SafeAreaView>
         <View style={Spacing.bannerContainer}>
-
           <View style={Spacing.topLogo}>
             <Image
               source={require("../assets/check-logo.png")}
@@ -38,68 +38,68 @@ export default class RecycleScreen extends React.Component {
             />
           </View>
 
-       
-            <View style={Spacing.sectionOneScan}>
-              <View style={{alignSelf: "flex-start"}}>
-                <Text style={Typography.headerText}>Scan Recycle Receipt:</Text>
-              </View>
-              <ScanBanner 
-                square={<Entypo name="camera" style={Typography.cameraEntypo}/>}    
-                navigate={this.photo}
-              />
-            </View>  
+          <View style={Spacing.sectionOneScan}>
+            <View style={{ alignSelf: "flex-start" }}>
+              <Text style={Typography.headerText}>Scan Recycle Receipt:</Text>
+            </View>
+            <ScanBanner
+              square={<Entypo name="camera" style={Typography.cameraEntypo} />}
+              navigate={this.photo}
+            />
+          </View>
 
-            <View style={Spacing.sectionTwoScan}>
-              <View style={{alignSelf: "flex-start"}}>
-                <Text style={Typography.headerText}>Results of Scan:</Text>
-              </View>
-
-              <View style={Spacing.scanResults}>
-                <Text style={Typography.quantityText}>Bottles Recycled: {this.state.recycled}</Text>
-                <Text style={Typography.quantityText}>Redeemable Amount: ${this.state.redeemableAmount} </Text>
-              </View>
+          <View style={Spacing.sectionTwoScan}>
+            <View style={{ alignSelf: "flex-start" }}>
+              <Text style={Typography.headerText}>Results of Scan:</Text>
             </View>
 
-
-            <View style={Spacing.sectionThreeScan}>
-              <View style={{alignSelf: "flex-start"}}>
-                <Text style={Typography.headerText}>Redeem for Celo:</Text>
-              </View>
-
-              <ActivityBanner
-                title="My Celo Wallet"
-                square={<Image 
-                  style={Spacing.celoLogo}
-                  source={require("../assets/celo-logo-colored-rings.png")} />}
-                  navigate={this.celo}
-              />  
+            <View style={Spacing.scanResults}>
+              <Text style={Typography.quantityText}>
+                Bottles Recycled: {this.state.recycled}
+              </Text>
+              <Text style={Typography.quantityText}>
+                Redeemable Amount: ${this.state.redeemableAmount}{" "}
+              </Text>
             </View>
           </View>
-          
-        
 
-       
-              
-      <View style={Spacing.bottomNavContainer}>
-        <View style={Spacing.bottomTextContainer}>
-          <TouchableOpacity onPress={this.logout}>
-            <Text style={Typography.linkText}>Click here to Log Out</Text>
-          </TouchableOpacity>
+          <View style={Spacing.sectionThreeScan}>
+            <View style={{ alignSelf: "flex-start" }}>
+              <Text style={Typography.headerText}>Redeem for Celo:</Text>
+            </View>
+
+            <ActivityBanner
+              title="My Celo Wallet"
+              square={
+                <Image
+                  style={Spacing.celoLogo}
+                  source={require("../assets/celo-logo-colored-rings.png")}
+                />
+              }
+              navigate={this.celo}
+            />
+          </View>
         </View>
-        
-        <View style={Spacing.bottomTextContainer}>
-          <Text style={Typography.footerText}>Learn more about Green Deeds </Text>
-          <TouchableOpacity onPress={this.about}>
-            <Text style={Typography.linkText}>here</Text>
-          </TouchableOpacity>
+
+        <View style={Spacing.bottomNavContainer}>
+          <View style={Spacing.bottomTextContainer}>
+            <TouchableOpacity onPress={this.logout}>
+              <Text style={Typography.linkText}>Click here to Log Out</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={Spacing.bottomTextContainer}>
+            <Text style={Typography.footerText}>
+              Learn more about Green Deeds{" "}
+            </Text>
+            <TouchableOpacity onPress={this.about}>
+              <Text style={Typography.linkText}>here</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-
-      </View>
-
       </SafeAreaView>
     );
   }
-
 }
 
 const styles = StyleSheet.create({
@@ -108,5 +108,3 @@ const styles = StyleSheet.create({
     height: 115,
   },
 });
-
-
